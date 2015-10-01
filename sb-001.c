a16ebaa7d45ea025cef5705ee4baccd7075edee3
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2015-10-01 11:15:05 +0400

    Made smarter "begin transaction"/"commit"

--- sb-001.c
+++ sb-001.c
@@ -678,15 +678,19 @@ static const char* filter_dev_type(unsigned char dev_id) {
 	return type_arr[dev_id];
 }
 
-static void filter_sql_sd(const char* key, const char* tbl, union CMD_BUFF* data) {
+static void filter_sql_sd(int* data_count, const char* key, const char* tbl, union CMD_BUFF* data) {
 	time_t t0 = data->sd.time;
 	struct tm* t1 = gmtime(&t0);
+	if(!*data_count) {
+		printf("begin transaction;\n");
+	}
 	printf("insert into %s values(\'%04d-%02d-%02d %02d:%02d:%02d\', \'%s-%d\', \'%g\', \'%s\', \'%s\');\n",
 		tbl, t1->tm_year + 1900, t1->tm_mon, t1->tm_mday, t1->tm_hour, t1->tm_min, t1->tm_sec,
 		filter_dev_name(data->sd.dev), data->sd.lun, data->sd.val, key, filter_dev_type(data->sd.dev));
+	(*data_count) ++;
 }
 
-static void filter_sql_fd(const char* key, const char* tbl, union CMD_BUFF* data, const char* outdir) {
+static void filter_sql_fd(int* data_count, const char* key, const char* tbl, union CMD_BUFF* data, const char* outdir) {
 	printf("commit;\n");
 	fflush(stdout);
 	if(strcmp("-", outdir)) {
@@ -696,7 +700,7 @@ static void filter_sql_fd(const char* key, const char* tbl, union CMD_BUFF* data
 		rename(".data", fname);
 		dup2(open(".data", O_CREAT | O_WRONLY), 1);
 	}
-	printf("begin transaction;\n");
+	(*data_count) = 0;
 }
 
 static int filter_sql(int argc, char* argv[]) {
@@ -716,12 +720,12 @@ static int filter_sql(int argc, char* argv[]) {
 		}
 		dup2(open(".data", O_CREAT | O_WRONLY), 1);
 	}
-	printf("begin transaction;\n");
 	fprintf(stderr, "filter.sql   ===>>\n");
+	int data_count = 0;
 	union CMD_BUFF data = {{0}};
 	while(0 < read(0, data.bt, sizeof(data.bt))) {
 		if(data.cmd >= CMD_LAST) continue;
-		arr[data.cmd](argv[1], argv[2], &data, argv[3]);
+		arr[data.cmd](&data_count, argv[1], argv[2], &data, argv[3]);
 	}
 	printf("commit;\n");
 	fflush(stdout);
